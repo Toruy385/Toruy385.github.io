e8f895a5df9c549a323c742b27f387732f252348
Toruy385 <76161357+Toruy385@users.noreply.github.com>
2022-08-13 02:36:33 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -43,3 +43,49 @@
     observer.observe(target);
   });
 }
+
+{
+  $(function() {
+    // スムーススクロール
+    // ページ内のリンクをクリックした時に動作する
+    $('a[href^="#"]').click(function() {
+      // リンクを取得
+      let href= $(this).attr("href");
+      // ジャンプ先のid名をセット
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      // topからジャンプ先の要素までを取得
+      let position = target.offset().top;
+      // animateでスムーススクロール
+      $("html, body").animate({scrollTop:position}, 600, "swing");
+      return false;
+    });
+
+    // ---------------------------------------
+    // topに戻る
+    let pagetop = $('#to-top');
+    // 最初に画面が表示されたときは、トップに戻るボタンを非表示に設定
+    pagetop.hide();
+
+    // スクロールイベント
+    $(window).scroll(function() {
+      // スクロールの位置が700pxを超えた場合
+      if ($(this).scrollTop() > 700) {
+        // トップに戻るボタンを表示
+        pagetop.fadeIn();
+
+        // スクロールが700px未満の場合
+      } else {
+        // トップに戻るボタンを非表示
+        pagetop.fadeOut();
+      }
+    });
+
+    // クリックイベント
+    pagetop.click(function() {
+      $('body,html').animate({scrollTop: 0}, 500);
+
+      // イベントが伝播しないための記述
+      return false;
+    });
+  });
+}
